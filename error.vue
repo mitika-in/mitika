ab61fc3c8974b7de0d8a877cccaec17ff3a1fe1a
Arun Mani J <j.arunmani@proton.me>
2025-07-13 20:12:56 +0530

    error: Show full error

--- error.vue
+++ error.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="flex h-dvh flex-col items-center justify-center gap-8 p-4">
-    <header class="flex flex-row items-center gap-4">
+    <header class="flex w-full flex-row items-center gap-4">
       <TitleBar
         class="grow"
         :subtitle="$t('An unexpected error occured')"
         :title="title"
       />
     </header>
-    <main class="@container flex max-w-lg flex-col gap-4">
+    <main class="@container flex w-full max-w-lg flex-col gap-4">
       <p>
         Something went wrong. You can try restarting the application and if the issue persists,
         please check the logs and report it to the developers.
       </p>
+      <textarea class="textarea max-h-[25vh] w-full font-mono">
+        {{ error }}
+      </textarea>
       <div class="flex flex-col items-center gap-4">
         <NuxtLink
           class="btn btn-primary w-full @lg:w-64"
